6cd463f3f752cf4118c8740ab0c8e8d3155ac7e9
MrHyaena <dolezalmartin131@gmail.com>
2024-11-06 15:46:18 +0100

    Collision solution, same key = update value solut

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 
 function control(index) {
-  if (index < 0 || index >= hmap.buckets.length) {
+  if (index < 0 || index > hmap.buckets.length) {
     throw new Error("Trying to access index out of bound");
   }
 }
@@ -14,29 +14,6 @@ class hashMap {
     this.loadfactor = 75 / 100;
     this.ratio = 0;
   }
-}
-
-// Function for inserting buckets into array
-
-function addBuckets(count) {
-  let bucketCount = count;
-  for (let i = 0; i < bucketCount; i++) {
-    hmap.buckets.push("");
-  }
-  hmap.counter = hmap.counter + bucketCount;
-}
-
-// HASH function
-
-function hash(key) {
-  let hashCode = 0;
-
-  const primeNumber = 31;
-  for (let i = 0; i < key.length; i++) {
-    hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % hmap.counter;
-  }
-  console.log(hashCode);
-  return hashCode;
 }
 
 // Functions for control and creating more buckets
@@ -55,36 +32,86 @@ function countratio() {
 }
 
 function morebuckets() {
-  if (hmap.ratio > hmap.loadfactor) {
-    addBuckets(16);
-
-    let oldarray = hmap.buckets;
-    hmap.buckets.map((item) => {
-      item = "";
-    });
-
+  let oldarray = hmap.buckets;
+  if (hmap.ratio >= hmap.loadfactor) {
+    hmap.buckets = [];
+    addBuckets(hmap.counter + 16);
+    console.log(oldarray);
     oldarray.map((item) => {
       if (item !== "") {
-        let first = item.key;
-        let second = item.value;
+        let first = "";
+        let second = "";
 
-        set(first, second);
+        if (item.length == 1) {
+          first = item[0].key;
+          second = item[0].value;
+          set(first, second);
+        } else {
+          item.map((inneritem) => {
+            first = inneritem.key;
+            second = inneritem.value;
+            set(first, second);
+          });
+        }
       }
     });
   }
 }
 
-// Basic set function
-
-function set(key, value) {
-  let itemhash = hash(key);
-  let index = itemhash;
-  control(index);
-
-  hmap.buckets[index] = { key: key, value: value };
+function addMoreBuckets() {
   counterFull();
   countratio();
   morebuckets();
+}
+
+// Function for inserting buckets into array
+
+function addBuckets(count) {
+  let bucketCount = count;
+  hmap.counter = 0;
+  for (let i = 0; i < bucketCount; i++) {
+    hmap.buckets.push("");
+  }
+  hmap.counter = hmap.counter + bucketCount;
+}
+
+// Basic set function
+
+function set(key, value) {
+  let index = hash(key);
+  addMoreBuckets();
+  control(index);
+
+  if (hmap.buckets[index] == "") {
+    hmap.buckets[index] = [{ key: key, value: value }];
+  } else {
+    let bucketarray = [];
+    hmap.buckets[index].map((item) => {
+      bucketarray.push(item.key);
+    });
+
+    if (bucketarray.includes(key)) {
+    } else {
+      let itemarray = hmap.buckets[index];
+      itemarray.push({ key: key, value: value });
+      console.log(itemarray);
+    }
+  }
+  console.log(hmap.counter);
+}
+
+// HASH function
+
+function hash(key) {
+  let hashCode = 0;
+  console.log(key.length);
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % hmap.counter;
+  }
+  console.log("hash " + hashCode);
+  console.log(hmap);
+  return hashCode;
 }
 
 let hmap = new hashMap();
@@ -100,13 +127,20 @@ set("frog", "green");
 set("grape", "purple");
 set("hat", "black");
 set("ice cream", "white");
-set("fdsafd", "blue");
-set("cum", "pink");
-set("ě", "green");
-set("č", "purple");
-set("é", "black");
-set("hcream", "white");
-set("2", "blue");
-set("1", "pink");
+set("hat", "white");
+
+newset();
+
+function newset() {
+  set("jacket", "blue");
+  set("kite", "pink");
+  set("lion", "golden");
+  set("dumbass", "purple");
+  set("meteor", "black");
+  set("stalagnite", "white");
+  set("pumpkin", "blue");
+  set("fight", "pink");
+  set("cat", "golden");
+}
 
 console.log(hmap);
