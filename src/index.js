9065f397532e83ab4092a61fce03342f98a28ca8
MrHyaena <dolezalmartin131@gmail.com>
2024-11-06 16:43:23 +0100

    Additional functions for wroking with hashmap

--- src/index.js
+++ src/index.js
@@ -19,10 +19,12 @@ class hashMap {
 // Functions for control and creating more buckets
 
 function counterFull() {
-  hmap.counterfull = 0;
+  hmap.counterfull = 1;
   hmap.buckets.map((item) => {
     if (item !== "") {
-      hmap.counterfull = hmap.counterfull + 1;
+      item.map(() => {
+        hmap.counterfull = hmap.counterfull + 1;
+      });
     }
   });
 }
@@ -36,7 +38,7 @@ function morebuckets() {
   if (hmap.ratio >= hmap.loadfactor) {
     hmap.buckets = [];
     addBuckets(hmap.counter + 16);
-    console.log(oldarray);
+
     oldarray.map((item) => {
       if (item !== "") {
         let first = "";
@@ -81,7 +83,6 @@ function set(key, value) {
   let index = hash(key);
   addMoreBuckets();
   control(index);
-
   if (hmap.buckets[index] == "") {
     hmap.buckets[index] = [{ key: key, value: value }];
   } else {
@@ -91,26 +92,27 @@ function set(key, value) {
     });
 
     if (bucketarray.includes(key)) {
+      hmap.buckets[index].map((item) => {
+        if (item.key == key) {
+          item.value = value;
+        }
+      });
     } else {
       let itemarray = hmap.buckets[index];
       itemarray.push({ key: key, value: value });
-      console.log(itemarray);
     }
   }
-  console.log(hmap.counter);
 }
 
 // HASH function
 
 function hash(key) {
   let hashCode = 0;
-  console.log(key.length);
+
   const primeNumber = 31;
   for (let i = 0; i < key.length; i++) {
     hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % hmap.counter;
   }
-  console.log("hash " + hashCode);
-  console.log(hmap);
   return hashCode;
 }
 
@@ -144,3 +146,124 @@ function newset() {
 }
 
 console.log(hmap);
+
+// Other functions
+
+console.log(get("hat"));
+console.log(has("cat"));
+console.log(remove("pumpkin"));
+console.log(hmap);
+console.log(length());
+
+function get(key) {
+  let value = null;
+  hmap.buckets.map((item) => {
+    if (item == "") {
+    } else {
+      item.map((inneritem) => {
+        if (inneritem.key == key) {
+          value = inneritem.value;
+        }
+      });
+    }
+  });
+  return value;
+}
+
+function has(key) {
+  let value = false;
+  hmap.buckets.map((item) => {
+    if (item == "") {
+    } else {
+      item.map((inneritem) => {
+        if (inneritem.key == key) {
+          value = true;
+        }
+      });
+    }
+  });
+  return value;
+}
+
+function remove(key) {
+  let value = false;
+  hmap.buckets.map((item) => {
+    if (item == "") {
+    } else {
+      item.map((inneritem) => {
+        if (inneritem.key == key) {
+          let index = item.indexOf(inneritem);
+          item.splice(index, 1);
+          value = true;
+          hmap.counterfull = hmap.counterfull - 1;
+        }
+      });
+    }
+  });
+  return value;
+}
+
+function length() {
+  return hmap.counterfull;
+}
+
+function clearing() {
+  hmap.buckets = [];
+  for (let i = 0; i < hmap.counter; i++) {
+    hmap.buckets.push([]);
+  }
+  hmap.counterfull = 0;
+  hmap.ratio = 0;
+  console.log(hmap);
+}
+
+console.log(keys());
+
+function keys() {
+  let keysArray = [];
+  hmap.buckets.map((item) => {
+    if (item == "") {
+    } else {
+      item.map((inneritem) => {
+        if (item.length > 0) {
+          keysArray.push(inneritem.key);
+        }
+      });
+    }
+  });
+  return keysArray;
+}
+
+console.log(value());
+
+function value() {
+  let keysArray = [];
+  hmap.buckets.map((item) => {
+    if (item == "") {
+    } else {
+      item.map((inneritem) => {
+        if (item.length > 0) {
+          keysArray.push(inneritem.value);
+        }
+      });
+    }
+  });
+  return keysArray;
+}
+
+console.log(entries());
+
+function entries() {
+  let keysArray = [];
+  hmap.buckets.map((item) => {
+    if (item == "") {
+    } else {
+      item.map((inneritem) => {
+        if (item.length > 0) {
+          keysArray.push(inneritem);
+        }
+      });
+    }
+  });
+  return keysArray;
+}
